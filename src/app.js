78ae7cf5536aafc7556ea37c07459c8ae8dea4d7
Dmitry Votchitsev <dmitry.votchitsev@gmail.com>
2022-10-05 15:10:04 +0200

    remove koa body

--- src/app.js
+++ src/app.js
@@ -1,10 +1,7 @@
 import Koa from 'koa';
-import koaBody from 'koa-body';
 import { messages } from './Data.js';
 
 const app = new Koa();
-
-app.use(koaBody());
 
 app.use(async ctx => {
   if (ctx.url === '/messages/unread') {
